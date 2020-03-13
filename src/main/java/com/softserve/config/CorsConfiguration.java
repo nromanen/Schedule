6387f1603ff2c8c967ed8ae137d262e189c82e96
andrewbatusha <andrew.batuiev@gmail.com>
2020-03-13 18:43:14 +0200

    -add cors properties

--- src/main/java/com/softserve/config/CorsConfiguration.java
+++ src/main/java/com/softserve/config/CorsConfiguration.java
@@ -1,19 +1,31 @@
 package com.softserve.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@PropertySource("classpath:cors.properties")
 public class CorsConfiguration {
+
+    private final Environment environment;
+
+    @Autowired
+    public CorsConfiguration(Environment environment) {
+        this.environment = environment;
+    }
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
-                registry.addMapping("/**").allowedOrigins("https://softservedemo.herokuapp.com/");
+                registry.addMapping("/**").allowedOrigins(environment.getProperty("cors.localurl"));
+                registry.addMapping("/**").allowedOrigins(environment.getProperty("cors.herokuUrl"));
             }
         };
     }
