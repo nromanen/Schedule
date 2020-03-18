3fb90bb32042ecf469683e565bcc35abd05fbf0f
Mazuryk Vasyl <vasja.mazurykgmail.com>
2020-03-18 13:21:01 +0200

    change pattern to email annotation

--- src/main/java/com/softserve/entity/User.java
+++ src/main/java/com/softserve/entity/User.java
@@ -4,8 +4,8 @@ import com.softserve.entity.enums.Role;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import javax.persistence.*;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
 
@@ -15,19 +15,18 @@ import java.io.Serializable;
 @Entity
 @Table(name = "users")
 public class User implements Serializable {
-    private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(columnDefinition = "serial")
     private long id;
 
-    @Pattern(regexp = EMAIL_PATTERN, message = "Wrong email")
+    @Email
+    @Size(max = 40)
     @Column(unique = true, length = 40)
     private String email;
 
-    @NotEmpty(message = "Name cannot be empty")
-    //@Size(min = 2, max = 32, message = "Name must be between 2 and 32 characters long")
+    @NotEmpty(message = "Password cannot be empty")
     private String password;
 
     @Enumerated(EnumType.STRING)
