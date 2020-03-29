d2e4b2140101f1a15138dce968ff9525ac9f262a
Mazuryk Vasyl <vasja.mazurykgmail.com>
2020-03-30 00:36:51 +0300

    fix

--- src/main/java/com/softserve/controller/TestSecurityController.java
+++ src/main/java/com/softserve/controller/TestSecurityController.java
@@ -2,6 +2,7 @@ package com.softserve.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,6 @@ public class TestSecurityController {
     @GetMapping
     @ApiOperation(value = "Test method")
     public ResponseEntity test() {
-        return ResponseEntity.ok("Successful security testing");
+        return ResponseEntity.status(HttpStatus.OK).body("Successful security testing");
     }
 }
