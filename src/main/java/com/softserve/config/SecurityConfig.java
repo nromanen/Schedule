0ff0b6b51209b006b09590e25ee9749d5e0d6118
Mazuryk Vasyl <vasja.mazurykgmail.com>
2020-03-30 09:17:56 +0300

    refactor

--- src/main/java/com/softserve/config/SecurityConfig.java
+++ src/main/java/com/softserve/config/SecurityConfig.java
@@ -13,11 +13,6 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
