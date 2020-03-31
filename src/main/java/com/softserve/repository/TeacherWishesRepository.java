4c04c7e6b1adf3b0eeaa1c5e5888aaa3c17048fd
Mazuryk Vasyl <vasja.mazurykgmail.com>
2020-03-31 13:39:34 +0300

    teacher wishes

--- src/main/java/com/softserve/repository/TeacherWishesRepository.java
+++ src/main/java/com/softserve/repository/TeacherWishesRepository.java
@@ -1,6 +1,8 @@
 package com.softserve.repository;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.softserve.entity.TeacherWishes;
 
 public interface TeacherWishesRepository extends BasicRepository<TeacherWishes, Long> {
+    void validateTeacherWish(JsonNode teacherWish);
 }
--- src/main/java/com/softserve/repository/impl/TeacherWishesRepositoryImpl.java
+++ src/main/java/com/softserve/repository/impl/TeacherWishesRepositoryImpl.java
@@ -1,10 +1,15 @@
 package com.softserve.repository.impl;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.softserve.entity.TeacherWishes;
 import com.softserve.entity.User;
+import com.softserve.entity.enums.EvenOdd;
+import com.softserve.exception.IncorrectWishException;
 import com.softserve.repository.TeacherWishesRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
+
+import java.time.DayOfWeek;
 
 
 @Repository
@@ -37,5 +42,39 @@ public class TeacherWishesRepositoryImpl extends BasicRepositoryImpl<TeacherWish
         entity = (TeacherWishes) sessionFactory.getCurrentSession().merge(entity);
         sessionFactory.getCurrentSession().update(entity);
         return entity;
+    }
+
+
+
+
+
+    private void validateTeacherWish(JsonNode teacherWish){
+        try {
+            teacherWish.forEach(jsonNode -> {
+                int dayOfWeek = jsonNode.get("day_of_week").asInt();
+                if(dayOfWeek == 0){
+                    throw new IncorrectWishException("day_of_week is incorrect, "+jsonNode);
+                }
+                for (JsonNode classNode : jsonNode.get("wishes")) {
+                    JsonNode classNumber = classNode.get("class_number");
+                    if (classNumber.asInt() == 0) {
+                        throw new IncorrectWishException("class_number is empty, "+ classNode);
+                    }
+                    JsonNode status = classNode.get("status");
+                    if (status.asText() == null) {
+                        throw new IncorrectWishException("status is empty, "+ classNode);
+                    }
+                }
+            });
+        }catch (Exception e) {
+            throw new IncorrectWishException("Wish is incorrect, "+ teacherWish.toString());
+        }
+    }
+
+
+
+    private String checkTeacherClassWish(long teacher_id, Long semesterId, DayOfWeek dayOfWeek, EvenOdd evenOdd, Long classId){
+
+        return "q";
     }
 }
