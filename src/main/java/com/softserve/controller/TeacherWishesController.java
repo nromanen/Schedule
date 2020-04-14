fc9e6352492dfacfc33be08bcda2d57ec21af36d
Vasja <vasja.mazuryk@gmail.com>
2020-04-14 12:46:48 +0000

    20 create wishlist functionality new

--- src/main/java/com/softserve/controller/TeacherWishesController.java
+++ src/main/java/com/softserve/controller/TeacherWishesController.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
@@ -53,5 +52,4 @@ public class TeacherWishesController {
         TeacherWishes teacherWishes = teacherWishesService.update(teacherWishesMapper.teacherWishesDTOToTeacherWishes(teacherWishesDTO));
         return ResponseEntity.status(HttpStatus.OK).body(teacherWishesMapper.teacherWishesToTeacherWishesDTO(teacherWishes));
     }
-
 }
