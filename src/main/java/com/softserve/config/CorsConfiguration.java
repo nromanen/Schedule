67099ad4561b28cb20e42f89f98b9acd35bb326c
Mazuryk Vasyl <vasja.mazurykgmail.com>
2020-04-17 16:58:52 +0300

    refactor

--- src/main/java/com/softserve/config/CorsConfiguration.java
+++ src/main/java/com/softserve/config/CorsConfiguration.java
@@ -1,10 +1,8 @@
 package com.softserve.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,20 +10,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @PropertySource("classpath:cors.properties")
 public class CorsConfiguration {
 
-    private final Environment environment;
-
-    @Autowired
-    public CorsConfiguration(Environment environment) {
-        this.environment = environment;
-    }
-
-    @Bean
+   @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins(environment.getProperty("cors.localurl"),
-                        environment.getProperty("cors.herokuUrl")).allowedMethods("GET","POST","PUT", "DELETE");
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET","POST","PUT", "DELETE");
             }
         };
     }
