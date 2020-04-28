3dcb9cf38e2e45970e5b76710edc9ffee390e6d2
Mazuryk Vasyl <vasja.mazurykgmail.com>
2020-04-29 01:25:45 +0300

    fix group update

--- src/main/java/com/softserve/service/GroupService.java
+++ src/main/java/com/softserve/service/GroupService.java
@@ -4,7 +4,8 @@ import com.softserve.entity.Group;
 import java.util.List;
 
 public interface GroupService extends BasicService<Group, Long> {
-    boolean isGroupExistsWitTitle(String title);
+    boolean isGroupExistsWithTitleAndIgnoreWithId(Long id, String title);
+    boolean isGroupExistsWithTitle(String title);
     boolean isExistsWithId(Long id);
     List<Group> getDisabled();
 
