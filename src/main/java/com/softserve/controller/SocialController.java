a11feef7ca55d7e49a5e7c0144f2d9d10cdebafc
Peter <petrofediuk004@gmail.com>
2020-04-28 16:49:07 +0300

    Added oauth 2 social

--- src/main/java/com/softserve/controller/SocialController.java
+++ src/main/java/com/softserve/controller/SocialController.java
@@ -1,6 +1,8 @@
 package com.softserve.controller;
 
 import com.softserve.dto.MessageDTO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,11 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/social")
+@Api(tags = "Social API")
 @Slf4j
 public class SocialController {
 
     @GetMapping("/loginSuccess")
+    @ApiOperation(value = "Get token after successful sign in via social network")
     public ResponseEntity<MessageDTO> getLoginInfo(@RequestParam("token") String token) {
+        log.info("Enter into getLoginInfo method");
         return ResponseEntity.ok().body(new MessageDTO(token));
     }
 }
