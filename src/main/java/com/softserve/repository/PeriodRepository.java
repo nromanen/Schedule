3f7e294033326f6cf948827de2bf1471e6ddaeb0
Fedir Havrylitsa <fedir_@i.ua>
2020-04-30 11:10:00 +0000

    Semester

--- src/main/java/com/softserve/repository/PeriodRepository.java
+++ src/main/java/com/softserve/repository/PeriodRepository.java
@@ -1,7 +1,10 @@
 package com.softserve.repository;
 import com.softserve.entity.Period;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface PeriodRepository extends BasicRepository<Period, Long> {
     Optional<Period> findByName(String name);
+    List<Period> getFistFourPeriods();
 }
