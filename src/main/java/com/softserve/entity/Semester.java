380f6cb4c7c5e0c06fb6e2e7a3b014c85485417a
Folf18 <Fedir_@i.ua>
2020-04-30 23:15:14 +0300

    Fix period order in semester

--- src/main/java/com/softserve/entity/Semester.java
+++ src/main/java/com/softserve/entity/Semester.java
@@ -66,10 +66,10 @@ public class Semester implements Serializable {
     @Enumerated(EnumType.STRING)
     @CollectionTable(name="semester_day")
     @Column(name = "day")
-    //@NotNull(message = "At least one day should persist in the semester")
+    @NotNull(message = "At least one day should persist in the semester")
     private Set<DayOfWeek> daysOfWeek;
 
-    //@NotNull(message = "Semester should contain at least one period")
+    @NotNull(message = "Semester should contain at least one period")
     @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(name = "semester_period",
             joinColumns = { @JoinColumn(name = "semester_id")},
