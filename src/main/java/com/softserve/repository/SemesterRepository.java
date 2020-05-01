e5d2a4d4d42b72a9ad5e67b9c0f4cb9263835833
Fedir Havrylitsa <fedir_@i.ua>
2020-05-01 11:32:43 +0000

    Semester updates

--- src/main/java/com/softserve/repository/SemesterRepository.java
+++ src/main/java/com/softserve/repository/SemesterRepository.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface SemesterRepository extends BasicRepository<Semester, Long> {
-    Optional<Semester> semesterDuplicates(Semester semester);
+    Long semesterDuplicates(String description, int year);
     Optional<Semester> getCurrentSemester();
     List<Semester> getDisabled();
     int setCurrentSemesterToFalse();
