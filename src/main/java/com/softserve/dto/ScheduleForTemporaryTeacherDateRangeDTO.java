322ce136f8f6d70371f082a0029b1ac18e5e8012
Mazuryk Vasyl <vasja.mazurykgmail.com>
2020-06-22 14:55:50 +0300

    +

--- src/main/java/com/softserve/dto/ScheduleForTemporaryTeacherDateRangeDTO.java
+++ src/main/java/com/softserve/dto/ScheduleForTemporaryTeacherDateRangeDTO.java
@@ -1,6 +1,5 @@
 package com.softserve.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,6 +12,5 @@ import java.util.List;
 @NoArgsConstructor
 @ToString
 public class ScheduleForTemporaryTeacherDateRangeDTO {
-    @JsonProperty(value = "class")
     private List<LessonTemporaryTeacherDTO> lessons;
 }
