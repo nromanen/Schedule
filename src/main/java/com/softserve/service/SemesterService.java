1a1bd1dfffee4ed0929b03971607ba806241265b
Serhiikon <kondrserg@gmail.com>
2021-07-16 22:19:04 +0300

    add "default" property to semester

--- src/main/java/com/softserve/service/SemesterService.java
+++ src/main/java/com/softserve/service/SemesterService.java
@@ -7,5 +7,7 @@ public interface SemesterService extends BasicService<Semester, Long> {
     Semester getCurrentSemester();
     List<Semester> getDisabled();
     Semester changeCurrentSemester(Long semesterId);
+    Semester getDefaultSemester();
+    Semester changeDefaultSemester(Long semesterId);
 
 }
