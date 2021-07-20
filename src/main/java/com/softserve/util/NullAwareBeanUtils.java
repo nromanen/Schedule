9941650dbc7fd3b6037c00311bcd0a8f9ebaada4
MaxZhubya <mazim.zh2001@gmail.com>
2021-07-20 17:11:59 +0300

    #61 - Changes based on comments.

--- src/main/java/com/softserve/util/NullAwareBeanUtils.java
+++ src/main/java/com/softserve/util/NullAwareBeanUtils.java
@@ -5,7 +5,6 @@ import org.apache.commons.beanutils.BeanUtilsBean;
 import java.lang.reflect.InvocationTargetException;
 
 public class NullAwareBeanUtils extends BeanUtilsBean {
-
     @Override
     public void copyProperty(Object dest, String name, Object value)
             throws IllegalAccessException, InvocationTargetException {
