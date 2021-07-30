9d95ad4fc7da9711a42a701f667e650a012ab40d
Ivan Bodnariuk <vbforwork702@gmail.com>
2021-07-30 06:26:45 +0300

    Add save() and update() for TeacherDTO to TeacherService

--- src/main/java/com/softserve/service/TeacherService.java
+++ src/main/java/com/softserve/service/TeacherService.java
@@ -1,13 +1,24 @@
 package com.softserve.service;
 
+import com.softserve.dto.TeacherDTO;
 import com.softserve.entity.Teacher;
 import java.util.List;
 
 public interface TeacherService extends BasicService<Teacher, Long> {
+    Teacher save(TeacherDTO teacherDTO);
+
+    Teacher update(TeacherDTO teacherDTO);
+
     Teacher joinTeacherWithUser(Long teacherId, Long userId);
+
     List<Teacher> getAllTeachersWithWishes();
+
     Teacher getTeacherWithWishes(Long id);
+
     List<Teacher> getDisabled();
+
     Teacher findByUserId(int userId);
+
     List<Teacher> getAllTeacherWithoutUser();
 }
+
