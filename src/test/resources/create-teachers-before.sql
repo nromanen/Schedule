684b765f6e52bfce6aee6635ee67c905a5aefbff
Ivan Bodnariuk <vbforwork702@gmail.com>
2021-08-02 16:29:23 +0300

    Refactor create-teachers-before.sql

--- src/test/resources/create-teachers-before.sql
+++ src/test/resources/create-teachers-before.sql
@@ -1,15 +1,15 @@
-delete from "schedule-test".public.lessons;
-delete from "schedule-test".public.teacher_wishes;
-delete from "schedule-test".public.teachers;
-delete from "schedule-test".public.department;
-delete from "schedule-test".public.users;
+delete from lessons;
+delete from teacher_wishes;
+delete from teachers;
+delete from department;
+delete from users;
 
-insert into "schedule-test".public.department(id, name) values (20, 'Department1');
+insert into department(id, name) values (20, 'Department1');
 
-insert into "schedule-test".public.users(id, email, password) values (20, 'teacher@gmail.com', 'Pass1233!');
+insert into users(id, email, password) values (20, 'teacher@gmail.com', 'Pass1233!');
 
-insert into "schedule-test".public.teachers(id, name, patronymic, position, surname, department_id, user_id)
+insert into teachers(id, name, patronymic, position, surname, department_id, user_id)
 values (1, 'Ivan', 'Ivanovych', 'docent', 'Ivanov', 20, 20);
 
-insert into "schedule-test".public.teachers(id, name, patronymic, position, surname, department_id, disable)
+insert into teachers(id, name, patronymic, position, surname, department_id, disable)
 values (2, 'Ivan', 'Ivanovych', 'docent', 'Ivanov', 20, true);
