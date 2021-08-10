d128fbc1ae5510f98f0487dcf7ada8348ab671d4
Ivan Bodnariuk <vbforwork702@gmail.com>
2021-08-10 15:51:35 +0300

    Refactor TeacherControllerTest

--- src/test/java/com/softserve/controller/TeacherControllerTest.java
+++ src/test/java/com/softserve/controller/TeacherControllerTest.java
@@ -1,13 +1,20 @@
 package com.softserve.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.softserve.assertions.CustomMockMvcAssertions;
 import com.softserve.config.DBConfigTest;
 import com.softserve.config.MyWebAppInitializer;
 import com.softserve.config.WebMvcConfig;
 import com.softserve.dto.DepartmentDTO;
 import com.softserve.dto.TeacherDTO;
 import com.softserve.dto.TeacherForUpdateDTO;
+import com.softserve.exception.apierror.ApiValidationError;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
@@ -17,30 +24,33 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.rules.SpringClassRule;
+import org.springframework.test.context.junit4.rules.SpringMethodRule;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import static org.hamcrest.Matchers.hasSize;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import java.util.Arrays;
+import java.util.List;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Category(IntegrationTestCategory.class)
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(JUnitParamsRunner.class)
 @ContextConfiguration(classes = {WebMvcConfig.class, DBConfigTest.class, MyWebAppInitializer.class})
 @WebAppConfiguration
 @WithMockUser(username = "first@mail.com", password = "$2a$04$SpUhTZ/SjkDQop/Zvx1.seftJdqvOploGce/wau247zQhpEvKtz9.", roles = "MANAGER")
 @Sql(value = "classpath:create-teachers-before.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @Sql(value = "classpath:delete-teachers-after.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class TeacherControllerTest {
+    @ClassRule
+    public static final SpringClassRule scr = new SpringClassRule();
+
+    @Rule
+    public final SpringMethodRule smr = new SpringMethodRule();
+
     private MockMvc mockMvc;
 
     private final ObjectMapper objectMapper = new ObjectMapper();
@@ -53,6 +63,8 @@ public class TeacherControllerTest {
 
     private TeacherForUpdateDTO teacherForUpdateDtoWithId2LAndWithoutEmail;
 
+    private CustomMockMvcAssertions assertions;
+
     @Autowired
     private WebApplicationContext wac;
 
@@ -62,8 +74,10 @@ public class TeacherControllerTest {
                 .apply(SecurityMockMvcConfigurers.springSecurity())
                 .build();
 
+        assertions = new CustomMockMvcAssertions(mockMvc, objectMapper, "/teachers");
+
         DepartmentDTO departmentDTO = new DepartmentDTO();
-        departmentDTO.setId(20L);
+        departmentDTO.setId(1L);
         departmentDTO.setName("Department1");
 
         String teacherName = "Ivan";
@@ -108,37 +122,19 @@ public class TeacherControllerTest {
     }
 
     @Test
-    public void getAllTeachers() throws Exception {
-        assertThatByUrlReturnedListWithOneTeacher("/teachers", teacherDtoWithId1L);
-    }
-
-    @Test
-    public void getAllTeachersWithWishes() throws Exception {
-        mockMvc.perform(get("/teachers/with-wishes").accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json"));
-    }
-
-    @Test
-    public void getTeacherWithWishesById() throws Exception {
-        mockMvc.perform(get("/teachers/{id}/with-wishes", 1).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json"));
-    }
-
-    @Test
     public void getTeacherById() throws Exception {
-        TeacherDTO expected = teacherDtoWithId1L;
-        mockMvc.perform(get("/teachers/{id}", 1).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json"))
-                .andExpect(matchTeacher(expected));
+        assertions.assertForGet(teacherDtoWithId1L, "/teachers/1");
+    }
+
+    @Test
+    public void getAllTeachers() throws Exception {
+        assertions.assertForGetListWithOneEntity(teacherDtoWithId1L);
     }
 
     @Test
     @WithMockUser(username = "first@mail.com", password = "$2a$04$SpUhTZ/SjkDQop/Zvx1.seftJdqvOploGce/wau247zQhpEvKtz9.", roles = "USER")
     public void returnForbiddenIfAuthenticatedUserRoleIsNotManager() throws Exception {
-        mockMvc.perform(get("/teachers/{id}", 1).contentType(MediaType.APPLICATION_JSON))
+        mockMvc.perform(get("/teachers").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isForbidden());
     }
 
@@ -146,126 +142,178 @@ public class TeacherControllerTest {
     public void saveTeacher() throws Exception {
         TeacherDTO expected = disabledTeacherDtoWithId2LAndWithoutEmail;
         expected.setId(null);
-        mockMvc.perform(post("/teachers").content(objectMapper.writeValueAsString(expected))
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated())
-                .andExpect(matchTeacherExcludingId(expected));
+        assertions.assertForSave(expected, TeacherControllerTest::matchTeacherExcludingId);
     }
 
     @Test
     public void updateTeacherIfEmailAndUserIdNotExist() throws Exception {
-        TeacherForUpdateDTO expected = teacherForUpdateDtoWithId2LAndWithoutEmail;
-        mockMvc.perform(put("/teachers").content(objectMapper.writeValueAsString(expected))
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(matchTeacherForUpdate(expected));
+        assertions.assertForUpdate(teacherForUpdateDtoWithId2LAndWithoutEmail);
     }
 
     @Test
     public void updateTeacherIfEmailAndUserIdExist() throws Exception {
-        TeacherForUpdateDTO expected = teacherForUpdateDtoWithId1L;
-        mockMvc.perform(put("/teachers").content(objectMapper.writeValueAsString(expected))
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(matchTeacherForUpdate(expected));
+        assertions.assertForUpdate(teacherForUpdateDtoWithId1L);
     }
 
     @Test
     public void deleteTeacher() throws Exception {
-        mockMvc.perform(delete("/teachers/{id}", 1)
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+        assertions.assertForDelete(1);
     }
 
     @Test
     public void returnNotFoundIfTeacherNotFoundedById() throws Exception {
-        mockMvc.perform(get("/teachers/100")).andExpect(status().isNotFound());
-    }
-
-    @Test
-    public void returnNotFoundIfTeacherWithWishesNotFoundedById() throws Exception {
-        mockMvc.perform(get("/teachers/100/with-wishes")).andExpect(status().isNotFound());
-    }
-
-    @Test
-    public void returnBadRequestIfSavedPositionIsNull() throws Exception {
-        TeacherDTO teacherDTO = teacherDtoWithId1L;
-        teacherDTO.setId(null);
-        teacherDTO.setPosition(null);
-        mockMvc.perform(post("/teachers").content(objectMapper.writeValueAsString(teacherDTO))
-                .contentType(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(status().isBadRequest());
-    }
-
-    @Test
-    public void returnBadRequestIfUpdatedNameIsNull() throws Exception {
-        TeacherDTO teacherDTO = teacherDtoWithId1L;
-        teacherDTO.setName(null);
-        mockMvc.perform(put("/teachers", 1).content(objectMapper.writeValueAsString(teacherDTO))
-                .contentType(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(status().isBadRequest());
+        assertions.assertForGetWhenEntityNotFound(100);
     }
 
     @Test
     public void getAllPublicTeachers() throws Exception {
-        assertThatByUrlReturnedListWithOneTeacher("/public/teachers", teacherDtoWithId1L);
+        assertions.assertForGetListWithOneEntity(teacherDtoWithId1L, "/public/teachers");
     }
 
     @Test
     public void getDisableTeachers() throws Exception {
-        assertThatByUrlReturnedListWithOneTeacher("/teachers/disabled", disabledTeacherDtoWithId2LAndWithoutEmail);
+        assertions.assertForGetListWithOneEntity(disabledTeacherDtoWithId2LAndWithoutEmail, "/teachers/disabled");
     }
 
     @Test
     public void getAllNotRegisteredTeachers() throws Exception {
-        assertThatByUrlReturnedListWithOneTeacher("/not-registered-teachers",
-                                                  disabledTeacherDtoWithId2LAndWithoutEmail);
+        assertions.assertForGetListWithOneEntity(disabledTeacherDtoWithId2LAndWithoutEmail, "/not-registered-teachers");
     }
 
-    private static ResultMatcher matchTeacher(TeacherDTO expected) {
-        return matchTeacher("$", expected);
-    }
-
-    private static ResultMatcher matchTeacher(String prefix, TeacherDTO expected) {
-        return ResultMatcher.matchAll(
-                jsonPath(prefix + ".id").value(expected.getId()),
-                matchTeacherExcludingId(prefix, expected)
+    public Object[] parametersForTestValidationException() {
+        TeacherDTO teacherDTOWithNullValues = new TeacherDTO();
+        ApiValidationError nameIsNull = new ApiValidationError(
+                "Teacher",
+                "name",
+                null,
+                "Name cannot be empty"
         );
+        ApiValidationError positionIsNull = new ApiValidationError(
+                "Teacher",
+                "position",
+                null,
+                "Position cannot be empty"
+        );
+        ApiValidationError surnameIsNull = new ApiValidationError(
+                "Teacher",
+                "surname",
+                null,
+                "Surname cannot be empty"
+        );
+        ApiValidationError patronymicIsNull = new ApiValidationError(
+                "Teacher",
+                "patronymic",
+                null,
+                "Patronymic cannot be empty"
+        );
+        List<ApiValidationError> errorListWithNullValues = Arrays.asList(
+                nameIsNull,
+                positionIsNull,
+                surnameIsNull,
+                patronymicIsNull
+        );
+
+        String wordWithLength1 = "T";
+
+        TeacherDTO teacherDTOWithValuesLengthsLessThanMin = new TeacherDTO();
+        teacherDTOWithValuesLengthsLessThanMin.setName(wordWithLength1);
+        teacherDTOWithValuesLengthsLessThanMin.setSurname(wordWithLength1);
+        teacherDTOWithValuesLengthsLessThanMin.setPosition(wordWithLength1);
+        teacherDTOWithValuesLengthsLessThanMin.setPatronymic(wordWithLength1);
+
+        ApiValidationError nameLengthIs1 = new ApiValidationError(
+                "Teacher",
+                "name",
+                wordWithLength1,
+                "Name must be between 2 and 35 characters long"
+        );
+
+        ApiValidationError positionLengthIs1 = new ApiValidationError(
+                "Teacher",
+                "position",
+                wordWithLength1,
+                "Position must be between 2 and 35 characters long"
+        );
+        ApiValidationError surnameLengthIs1 = new ApiValidationError(
+                "Teacher",
+                "surname",
+                wordWithLength1,
+                "Surname must be between 2 and 35 characters long"
+        );
+        ApiValidationError patronymicLengthIs1 = new ApiValidationError(
+                "Teacher",
+                "patronymic",
+                wordWithLength1,
+                "Patronymic must be between 2 and 35 characters long"
+        );
+        List<ApiValidationError> errorListWithMinLength = Arrays.asList(
+                nameLengthIs1,
+                positionLengthIs1,
+                surnameLengthIs1,
+                patronymicLengthIs1
+        );
+
+        String wordWithLength36 = RandomStringUtils.random(36, "abc");
+
+        TeacherDTO teacherDTOWithValuesLengthsMoreThanMax = new TeacherDTO();
+        teacherDTOWithValuesLengthsMoreThanMax.setName(wordWithLength36);
+        teacherDTOWithValuesLengthsMoreThanMax.setSurname(wordWithLength36);
+        teacherDTOWithValuesLengthsMoreThanMax.setPosition(wordWithLength36);
+        teacherDTOWithValuesLengthsMoreThanMax.setPatronymic(wordWithLength36);
+
+        ApiValidationError nameLengthIs36 = new ApiValidationError(
+                "Teacher",
+                "name",
+                wordWithLength36,
+                "Name must be between 2 and 35 characters long"
+        );
+
+        ApiValidationError positionLengthIs36 = new ApiValidationError(
+                "Teacher",
+                "position",
+                wordWithLength36,
+                "Position must be between 2 and 35 characters long"
+        );
+        ApiValidationError surnameLengthIs36 = new ApiValidationError(
+                "Teacher",
+                "surname",
+                wordWithLength36,
+                "Surname must be between 2 and 35 characters long"
+        );
+        ApiValidationError patronymicLengthIs36 = new ApiValidationError(
+                "Teacher",
+                "patronymic",
+                wordWithLength36,
+                "Patronymic must be between 2 and 35 characters long"
+        );
+        List<ApiValidationError> errorListWithMaxLength = Arrays.asList(
+                nameLengthIs36,
+                positionLengthIs36,
+                surnameLengthIs36,
+                patronymicLengthIs36
+        );
+
+        return new Object[] {
+                new Object[] { teacherDTOWithNullValues, errorListWithNullValues},
+                new Object[] { teacherDTOWithValuesLengthsLessThanMin, errorListWithMinLength },
+                new Object[] { teacherDTOWithValuesLengthsMoreThanMax, errorListWithMaxLength }
+        };
+    }
+
+    @Parameters
+    @Test
+    public void testValidationException(TeacherDTO teacherDTO, List<ApiValidationError> errorList) throws Exception {
+        assertions.assertForValidationErrorsOnSave(errorList, teacherDTO);
     }
 
     private static ResultMatcher matchTeacherExcludingId(TeacherDTO expected) {
-        return matchTeacherExcludingId("$", expected);
-    }
-
-    private static ResultMatcher matchTeacherExcludingId(String prefix, TeacherDTO expected) {
         return ResultMatcher.matchAll(
-                jsonPath(prefix + ".name").value(expected.getName()),
-                jsonPath(prefix + ".surname").value(expected.getSurname()),
-                jsonPath(prefix + ".patronymic").value(expected.getPatronymic()),
-                jsonPath(prefix + ".position").value(expected.getPosition()),
-                jsonPath(prefix + ".department").value(expected.getDepartmentDTO())
-        );
-    }
-
-    private static ResultMatcher matchTeacherForUpdate(TeacherForUpdateDTO expected) {
-        return ResultMatcher.matchAll(
-                jsonPath("$.id").value(expected.getId()),
                 jsonPath("$.name").value(expected.getName()),
                 jsonPath("$.surname").value(expected.getSurname()),
                 jsonPath("$.patronymic").value(expected.getPatronymic()),
                 jsonPath("$.position").value(expected.getPosition()),
                 jsonPath("$.department").value(expected.getDepartmentDTO())
         );
-    }
-
-    private void assertThatByUrlReturnedListWithOneTeacher(String url, TeacherDTO expected) throws Exception {
-        mockMvc.perform(get(url).accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json"))
-                .andExpect(jsonPath("$", hasSize(1)))
-                .andExpect(matchTeacher("$[0]", expected));
     }
 }
 
--- src/test/resources/create-teachers-before.sql
+++ src/test/resources/create-teachers-before.sql
@@ -4,12 +4,12 @@ delete from teachers;
 delete from department;
 delete from users;
 
-insert into department(id, name) values (20, 'Department1');
+insert into department(id, name) values (1, 'Department1');
 
-insert into users(id, email, password) values (20, 'teacher@gmail.com', 'Pass1233!');
+insert into users(id, email, password) values (1, 'teacher@gmail.com', 'Pass1233!');
 
 insert into teachers(id, name, patronymic, position, surname, department_id, user_id)
-values (1, 'Ivan', 'Ivanovych', 'docent', 'Ivanov', 20, 20);
+values (1, 'Ivan', 'Ivanovych', 'docent', 'Ivanov', 1, 1);
 
 insert into teachers(id, name, patronymic, position, surname, department_id, disable)
-values (2, 'Ivan', 'Ivanovych', 'docent', 'Ivanov', 20, true);
+values (2, 'Ivan', 'Ivanovych', 'docent', 'Ivanov', 1, true);
