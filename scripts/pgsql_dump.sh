e21acf30996734354030475475e40ea5b10bad9e
Ivan <ivankozmulyak@gmail.com>
2021-08-16 18:31:43 +0300

    change scripts for Heroku

--- scripts/pgsql_dump.sh
+++ scripts/pgsql_dump.sh
@@ -5,16 +5,20 @@ pathA=/c/Program\ Files/PostgreSQL/13/bin
 export PATH=$PATH:$pathA
 
 #change on your password
-PGPASSWORD=admin
+PGPASSWORD=7f4837d6a03ee3ca96073a0a0fb295256f4e2f193ae5c8a730d787ae736df19a
 export PGPASSWORD
 
 #change to the path where the dump will be saved
 pathB=../backup
 #change on your user
-dbUser=postgres
+dbUser=fmwcxyhjckqwga
 #change on your database
-database=schedule
+database=d27evucpfis4qe
+#change on your host
+host=ec2-52-17-1-206.eu-west-1.compute.amazonaws.com
+#change on your port
+port=5432
 
-pg_dump -U $dbUser $database > $pathB/pgsql_$(date "+%Y-%m-%d-%H-%M").dump
+pg_dump --no-owner -U $dbUser -h $host -p $port -d $database > $pathB/pgsql_$(date "+%Y-%m-%d-%H-%M").dump
 
 unset PGPASSWORD
--- scripts/pgsql_restore.sh
+++ scripts/pgsql_restore.sh
@@ -5,20 +5,23 @@ pathA=/c/Program\ Files/PostgreSQL/13/bin
 export PATH=$PATH:$pathA
 
 #change on your password
-PGPASSWORD=admin
+PGPASSWORD=7f4837d6a03ee3ca96073a0a0fb295256f4e2f193ae5c8a730d787ae736df19a
 export PGPASSWORD
 
 #change the path to the file from which will be made restore
 pathB=../backup/
 #change to name of the file from which will be made restore
-filename=pgsql_2021-07-21-18-18.dump
+filename=pgsql_2021-08-16-18-17.dump
 #change on your user
-dbUser=postgres
+dbUser=fmwcxyhjckqwga
 #change on your database
-database=schedule
+database=d27evucpfis4qe
+#change on your host
+host=ec2-52-17-1-206.eu-west-1.compute.amazonaws.com
+#change on your port
+port=5432
 
-dropdb --if-exists -U $dbUser $database
-psql -U $dbUser -c "create database $database;"
-psql --set ON_ERROR_STOP=on -U $dbUser -d $database -1 -f $pathB$filename
+psql -U $dbUser -h $host -p $port -d $database -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
+psql --set ON_ERROR_STOP=off -U $dbUser -h $host -p $port -d $database -1 -f $pathB$filename
 
 unset PGPASSWORD
