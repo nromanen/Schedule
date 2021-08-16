33a45042d4377acd730473b82b9ce6120af96eb1
Ivan <ivankozmulyak@gmail.com>
2021-08-16 13:38:51 +0300

    change using Collect.toList

--- src/main/java/com/softserve/service/impl/GroupServiceImpl.java
+++ src/main/java/com/softserve/service/impl/GroupServiceImpl.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
