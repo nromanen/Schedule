c99a0afb1aae6b8d5025df46333f04ac9546b856
Ivan <ivankozmulyak@gmail.com>
2021-08-16 18:43:24 +0300

    change script

--- scripts/pgsql_dump.sh
+++ scripts/pgsql_dump.sh
@@ -5,19 +5,19 @@ pathA=/c/Program\ Files/PostgreSQL/13/bin
 export PATH=$PATH:$pathA
 
 #change on your password
-PGPASSWORD=7f4837d6a03ee3ca96073a0a0fb295256f4e2f193ae5c8a730d787ae736df19a
+PGPASSWORD=
 export PGPASSWORD
 
 #change to the path where the dump will be saved
 pathB=../backup
-#change on your user
-dbUser=fmwcxyhjckqwga
-#change on your database
-database=d27evucpfis4qe
-#change on your host
-host=ec2-52-17-1-206.eu-west-1.compute.amazonaws.com
-#change on your port
-port=5432
+#write your user
+dbUser=
+#write your database
+database=
+#write your host
+host=
+#write your port
+port=
 
 pg_dump --no-owner -U $dbUser -h $host -p $port -d $database > $pathB/pgsql_$(date "+%Y-%m-%d-%H-%M").dump
 
--- scripts/pgsql_restore.sh
+++ scripts/pgsql_restore.sh
@@ -4,22 +4,22 @@
 pathA=/c/Program\ Files/PostgreSQL/13/bin
 export PATH=$PATH:$pathA
 
-#change on your password
-PGPASSWORD=7f4837d6a03ee3ca96073a0a0fb295256f4e2f193ae5c8a730d787ae736df19a
+#write your password
+PGPASSWORD=
 export PGPASSWORD
 
 #change the path to the file from which will be made restore
 pathB=../backup/
 #write this on the command line as the first parameter
 filename=$1
-#change on your user
-dbUser=fmwcxyhjckqwga
-#change on your database
-database=d27evucpfis4qe
-#change on your host
-host=ec2-52-17-1-206.eu-west-1.compute.amazonaws.com
-#change on your port
-port=5432
+#write your user
+dbUser=
+#write your database
+database=
+#write your host
+host=
+#write your port
+port=
 
 psql -U $dbUser -h $host -p $port -d $database -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
 psql --set ON_ERROR_STOP=off -U $dbUser -h $host -p $port -d $database -1 -f $pathB$filename
