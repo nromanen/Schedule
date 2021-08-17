38c146847750ca93a8b789de5293ef94cd5344f0
nromanen <nromanen.softserveinc@gmail.com>
2021-08-17 20:08:59 +0300

    add frontend

---
+++ src/main/webapp/precache-manifest.3905501f49a417d61418e4f47fcd826f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b278b2947e6fd14f816230a07b9f70b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02c009b6cfb05f398036",
+    "url": "/static/css/main.192fc4ac.chunk.css"
+  },
+  {
+    "revision": "84fb00e78bb89b9713c4",
+    "url": "/static/js/2.af9ad698.chunk.js"
+  },
+  {
+    "revision": "fde8a65b8d307872c8c44f6de8ee07de",
+    "url": "/static/js/2.af9ad698.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02c009b6cfb05f398036",
+    "url": "/static/js/main.3aaa0102.chunk.js"
+  },
+  {
+    "revision": "d6634c61822753d83409",
+    "url": "/static/js/runtime-main.c3b6c623.js"
+  }
+]);
