6e15cb6fb306afbe01039bde0b372f11482c97e6
vadim <vadim.skuratovskij@gmail.com>
2021-08-18 17:56:36 +0300

    resolve issue#105

--- src/main/java/com/softserve/util/Translator.java
+++ src/main/java/com/softserve/util/Translator.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.ResourceUtils;
 
@@ -13,21 +14,11 @@ import java.util.Locale;
 import java.util.Map;
 
 @Slf4j
+@NoArgsConstructor
 public class Translator {
     private static Translator translator;
 
-    private final Map<String, Map<Locale, String>> dictionary;
-
-    public Translator() {
-        if(translator == null) {
-            translator = readTranslatorFromFile();
-        }
-        dictionary = translator.dictionary;
-    }
-
-    public Translator(Map<String, Map<Locale, String>> dictionary) {
-        this.dictionary = dictionary;
-    }
+    private Map<String, Map<Locale, String>> dictionary = new HashMap<>();
 
     public String getTranslation(String key, Locale language) {
         if(dictionary.containsKey(key) && dictionary.get(key).containsKey(language)) {
@@ -37,15 +28,16 @@ public class Translator {
         return key;
     }
 
-    private static Translator readTranslatorFromFile() {
-        Translator translator;
-        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-        mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
-        try {
-            translator = mapper.readValue(ResourceUtils.getFile("classpath:dictionary.yaml"), Translator.class);
-        } catch (IOException e) {
-            log.error("Error occurred while parsing file dictionary.yaml", e);
-            translator = new Translator(new HashMap<>());
+    public static Translator getInstance() {
+        if (translator == null) {
+            ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+            mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+            try {
+                translator = mapper.readValue(ResourceUtils.getFile("classpath:dictionary.yaml"), Translator.class);
+            } catch (IOException e) {
+                log.error("Error occurred while parsing file dictionary.yaml", e);
+                translator = new Translator();
+            }
         }
         return translator;
     }
