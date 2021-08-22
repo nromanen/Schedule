afd80da74e17edb13906b71a692b3eb08c9652c2
Nastia <gerasymchuk88888888@gmail.com>
2021-08-22 20:30:58 +0300

    code refactoring'

--- frontend/src/services/uploadFile.js
+++ frontend/src/services/uploadFile.js
@@ -3,14 +3,14 @@ import { STUDENTS_TO_GROUP_FILE } from '../constants/axios';
 import i18n from '../helper/i18n';
 import { successHandler } from '../helper/handlerAxios';
 
-export const uploadStudentsToGroupFile = (file,groupId) => {
+export const uploadStudentsToGroupFile = (file, groupId) => {
     const formData = new FormData();
-    formData.append("file", file);
+    formData.append('file', file);
     axios
         .post(`${STUDENTS_TO_GROUP_FILE}${groupId}`, formData)
         .then((res) => {
-            let students=res.data.length;
-            students+=students!==1?` ${i18n.t('formElements:students_file_label')}`:` ${i18n.t('formElements:student_file_label')}`;
+            let students = res.data.length;
+            students += students !== 1 ? ` ${i18n.t('formElements:students_file_label')}` : ` ${i18n.t('formElements:student_file_label')}`;
             successHandler(
                 i18n.t('serviceMessages:file_backend_success_operation', {
                     cardType: students,
@@ -18,5 +18,5 @@ export const uploadStudentsToGroupFile = (file,groupId) => {
                 })
             );
         })
-        .catch((err) => alert("File Upload Error"));
-}
+        .catch((err) => alert('File Upload Error'));
+};
--- frontend/src/share/modals/modal/showStudentsDialog.js
+++ frontend/src/share/modals/modal/showStudentsDialog.js
@@ -7,7 +7,7 @@ import Dialog from '@material-ui/core/Dialog';
 import Button from '@material-ui/core/Button';
 
 import '../dialog.scss';
-import './showDataDialog.scss'
+import './showDataDialog.scss';
 
 import i18n from '../../../helper/i18n';
 import { connect } from 'react-redux';
@@ -21,212 +21,213 @@ import { UploadFile } from '../../../components/UploadFile/UploadFile';
 
 
 export const ShowStudentsDialog = props => {
-    const { onClose,  cardId, open,onDeleteStudent,students,onSubmit,match,student,groups,group} = props;
-    const [checkBoxStudents,setCheckBoxStudents]=useState([]);
-    const [checkedAll,setCheckedAll]=useState(false);
-    const [openUploadFile,setOpenUploadFile]=useState(false);
-    const setDefaultGroupOption=()=>{
-        return {value: `${ group.id }`,label: `${ group.title }`,...group};
-    }
-    const [groupOption,setGroupOption]=useState({});
-    const [defaultGroup,setDefaultGroup]=useState({})
-    useEffect(()=>{
-        if(group.id!==null)
-        setDefaultGroup(setDefaultGroupOption());
-    },[group.id])
+    const { onClose, cardId, open, onDeleteStudent, students, onSubmit, match, student, groups, group } = props;
+    const [checkBoxStudents, setCheckBoxStudents] = useState([]);
+    const [checkedAll, setCheckedAll] = useState(false);
+    const [openUploadFile, setOpenUploadFile] = useState(false);
+    const setDefaultGroupOption = () => {
+        return { value: `${group.id}`, label: `${group.title}`, ...group };
+    };
+    const [groupOption, setGroupOption] = useState({});
+    const [defaultGroup, setDefaultGroup] = useState({});
+    useEffect(() => {
+        if (group.id !== null)
+            setDefaultGroup(setDefaultGroupOption());
+    }, [group.id]);
     const { t } = useTranslation('formElements');
-    useEffect(()=> {
+    useEffect(() => {
         getAllStudentsByGroupId(props.group.id);
 
 
-    },[open]);
-    useEffect(()=> {
+    }, [open]);
+    useEffect(() => {
         getAllStudentsByGroupId(props.group.id);
 
 
-    },[openUploadFile]);
-    useEffect(()=>{
+    }, [openUploadFile]);
+    useEffect(() => {
         parseStudentToCheckBox();
-    },[props.students]);
+    }, [props.students]);
 
-    const setGroupsOption=()=>{
-        return groups!==undefined? groups.map(item=>{return {value:item.id,label:`${item.title}`,...item}}):null;
+    const setGroupsOption = () => {
+        return groups !== undefined ? groups.map(item => {
+            return { value: item.id, label: `${item.title}`, ...item };
+        }) : null;
 
-    }
+    };
 
-    const groupsOption=setGroupsOption();
+    const groupsOption = setGroupsOption();
     const handleClose = () => {
         onClose(cardId);
     };
 
 
     const parseStudentToCheckBox = () => {
-        const studentsCheckBox=[...students];
-        const res=studentsCheckBox.map((item)=> {
-            return {...item,checked:false};
+        const studentsCheckBox = [...students];
+        const res = studentsCheckBox.map((item) => {
+            return { ...item, checked: false };
         });
         setCheckBoxStudents(res);
-    }
+    };
     const handleCheckElement = (event) => {
         let studentsTmp = [...checkBoxStudents];
         studentsTmp.forEach(student => {
-            if (student.id ===Number( event.target.value))
-                student.checked =  event.target.checked
-        })
+            if (student.id === Number(event.target.value))
+                student.checked = event.target.checked;
+        });
         setCheckBoxStudents(studentsTmp);
-    }
-   const handleAllChecked = (event,pageItemsCount,page,rowsPerPage) => {
+    };
+    const handleAllChecked = (event, pageItemsCount, page, rowsPerPage) => {
         let studentsTmp = [...checkBoxStudents];
-       for (let i = (page)*rowsPerPage; i <pageItemsCount+(page)*rowsPerPage ; i++) {
-           studentsTmp[i].checked = event.target.checked
-       }
-        setCheckBoxStudents(studentsTmp)
-    }
-    const handleAllCheckedBtn = (pageItemsCount,page,rowsPerPage) => {
+        for (let i = (page) * rowsPerPage; i < pageItemsCount + (page) * rowsPerPage; i++) {
+            studentsTmp[i].checked = event.target.checked;
+        }
+        setCheckBoxStudents(studentsTmp);
+    };
+    const handleAllCheckedBtn = (pageItemsCount, page, rowsPerPage) => {
         let studentsTmp = [...checkBoxStudents];
-        let start=(page)*rowsPerPage;
-        let finish=pageItemsCount+(page)*rowsPerPage;
-        while (start<finish){
-            if (studentsTmp[start].checked){
+        let start = (page) * rowsPerPage;
+        let finish = pageItemsCount + (page) * rowsPerPage;
+        while (start < finish) {
+            if (studentsTmp[start].checked) {
                 start++;
-            }
-            else {
+            } else {
                 break;
             }
         }
-        setCheckedAll(start===finish&&start!==0)
-    }
+        setCheckedAll(start === finish && start !== 0);
+    };
     const handleAllClear = () => {
         let studentsTmp = [...checkBoxStudents];
         studentsTmp.forEach(student => {
-                if(student.checked) {
-                    student.checked = false;
-                }
-        })
+            if (student.checked) {
+                student.checked = false;
+            }
+        });
         setCheckBoxStudents(studentsTmp);
-    }
+    };
     const handleChangeCheckedAllBtn = () => {
         setCheckedAll(prevState => !prevState);
-    }
+    };
     const handleClearCheckedAllBtn = () => {
         setCheckedAll(false);
-    }
+    };
 
 
     const setCurrentGroupOption = (group) => {
-      setGroupOption(group)
-    }
-    const isChecked=({checked})=>{
+        setGroupOption(group);
+    };
+    const isChecked = ({ checked }) => {
         return checked;
-    }
-    const setSelectDisabled=()=>{
-        let resChecked=true;
+    };
+    const setSelectDisabled = () => {
+        let resChecked = true;
         for (let i = 0; i < checkBoxStudents.length; i++) {
-            if(isChecked(checkBoxStudents[i])){
-                resChecked= false;
+            if (isChecked(checkBoxStudents[i])) {
+                resChecked = false;
                 break;
             }
         }
         return resChecked;
-    }
-    const setDisabledMoveToGroupBtn=()=>{
+    };
+    const setDisabledMoveToGroupBtn = () => {
         return setSelectDisabled();
-    }
-    const changeStudentItem = (group,student) => {
-        let resData={};
-        if(student.checked){
-            const {checked, ...res} = student;
-            resData= {...res,group:{id:group.id}}
+    };
+    const changeStudentItem = (group, student) => {
+        let resData = {};
+        if (student.checked) {
+            const { checked, ...res } = student;
+            resData = { ...res, group: { id: group.id } };
         }
         return resData;
-    }
+    };
     const handleSubmitGroupStudents = () => {
-        if(isObjectEmpty(groupOption)){
+        if (isObjectEmpty(groupOption)) {
             successHandler(
                 i18n.t('serviceMessages:students_exist_in_this_group', {
                     cardType: i18n.t('common:student_title'),
                     actionType: i18n.t('serviceMessages:student_label')
                 })
             );
-        }
-        else{
-            const {value,label,...res}=groupOption;
-            const currentStudents=[...checkBoxStudents];
-            const resData=[];
-            const prevGroup={id:defaultGroup.id};
+        } else {
+            const { value, label, ...res } = groupOption;
+            const currentStudents = [...checkBoxStudents];
+            const resData = [];
+            const prevGroup = { id: defaultGroup.id };
             for (let i = 0; i < currentStudents.length; i++) {
-                const resItem=changeStudentItem(res,currentStudents[i]);
-                if(!isObjectEmpty(resItem)){
-                    resData.push(resItem)
+                const resItem = changeStudentItem(res, currentStudents[i]);
+                if (!isObjectEmpty(resItem)) {
+                    resData.push(resItem);
                 }
-            };
-            resData.forEach(item=>updateStudentService({ ...item, prevGroup }));
+            }
+            ;
+            resData.forEach(item => updateStudentService({ ...item, prevGroup }));
         }
         handleClearCheckedAllBtn();
-    }
-    const handleOpenDialogFile=()=>{
-        setOpenUploadFile(true)
-    }
-    const handleCloseDialogFile=()=>{
-        setOpenUploadFile(false)
-    }
+    };
+    const handleOpenDialogFile = () => {
+        setOpenUploadFile(true);
+    };
+    const handleCloseDialogFile = () => {
+        setOpenUploadFile(false);
+    };
     return (
         <Dialog
             disableBackdropClick={true}
             onClose={handleClose}
-            aria-labelledby="confirm-dialog-title"
+            aria-labelledby='confirm-dialog-title'
             open={open}
         >
 
-            <DialogTitle id="confirm-dialog-title">
+            <DialogTitle id='confirm-dialog-title'>
                 <>
 
-                        <>
+                    <>
 
-                            {students.length === 0 ?
+                        {students.length === 0 ?
 
-                                <>
+                            <>
 
-                                    <h2 className="title-align">{`${t('group_label')} - `}<span>{`${props.group.title}`}</span>
-                                    </h2>
-                                    {t('no_exist_students_in_group')}
-                                </>
+                                <h2 className='title-align'>{`${t('group_label')} - `}<span>{`${props.group.title}`}</span>
+                                </h2>
+                                {t('no_exist_students_in_group')}
+                            </>
 
-                                :
-                                <span className="table-student-data">
+                            :
+                            <span className='table-student-data'>
                                     <h6>
                                       <Select
                                           className={'group-select'}
                                           defaultValue={defaultGroup}
                                           options={groupsOption}
                                           onChange={setCurrentGroupOption}
-                                          isOptionDisabled={()=>setSelectDisabled()}
+                                          isOptionDisabled={() => setSelectDisabled()}
                                       />
                                   </h6>
-                                <h3 className="title-align"><span>{students.length !== 1 ? `${t('students_label')} ` : `${t('student_label')} `}</span>{`${t('group_students')} `}<span>{`${props.group.title}`}</span></h3>
+                                <h3 className='title-align'><span>{students.length !== 1 ? `${t('students_label')} ` : `${t('student_label')} `}</span>{`${t('group_students')} `}<span>{`${props.group.title}`}</span></h3>
 
                                     <RenderStudentTable group={props.group} onDeleteStudent={onDeleteStudent}
-                                                    students={students} onSubmit={onSubmit}
-                                                    match={match}
-                                                    student={props.student}
-                                                    checkBoxStudents={checkBoxStudents}
-                                                    handleCheckElement={handleCheckElement}
-                                                    handleAllChecked={handleAllChecked}
-                                                    handleAllClear={handleAllClear}
-                                                    handleChangeCheckedAllBtn={handleChangeCheckedAllBtn}
-                                                    handleClearCheckedAllBtn={handleClearCheckedAllBtn}
-                                                    checkedAllBtn={checkedAll}
-                                                    handleAllCheckedBtn={handleAllCheckedBtn}
-                                />
+                                                        students={students} onSubmit={onSubmit}
+                                                        match={match}
+                                                        student={props.student}
+                                                        checkBoxStudents={checkBoxStudents}
+                                                        handleCheckElement={handleCheckElement}
+                                                        handleAllChecked={handleAllChecked}
+                                                        handleAllClear={handleAllClear}
+                                                        handleChangeCheckedAllBtn={handleChangeCheckedAllBtn}
+                                                        handleClearCheckedAllBtn={handleClearCheckedAllBtn}
+                                                        checkedAllBtn={checkedAll}
+                                                        handleAllCheckedBtn={handleAllCheckedBtn}
+                                    />
                             </span>
-                            }
-                        </>
+                        }
+                    </>
                 </>
             </DialogTitle>
-            <div className="buttons-container">
-                {<UploadFile group={group} open={openUploadFile} handleCloseDialogFile={handleCloseDialogFile}/>}
+            <div className='buttons-container'>
+                {<UploadFile group={group} open={openUploadFile} handleCloseDialogFile={handleCloseDialogFile} />}
                 <Button
-                    className={students.length !== 0?'student-dialog-button-data':'student-dialog-button-no-data'}
+                    className={students.length !== 0 ? 'student-dialog-button-data' : 'student-dialog-button-no-data'}
                     variant='contained'
                     onClick={handleOpenDialogFile}
                     color='primary'
@@ -234,23 +235,23 @@ export const ShowStudentsDialog = props => {
                 >
                     {i18n.t('common:upload_from_file_title')}
                 </Button>
-                {students.length !== 0?
+                {students.length !== 0 ?
+                    <Button
+                        className='student-dialog-button-data'
+                        variant='contained'
+                        onClick={handleSubmitGroupStudents}
+                        color='primary'
+                        disabled={setDisabledMoveToGroupBtn()}
+                        title={i18n.t('move_to_group_title')}
+                    >
+                        {i18n.t('common:move_to_group_title')}
+                    </Button>
+                    : null}
                 <Button
-                    className='student-dialog-button-data'
+                    className={students.length !== 0 ? 'student-dialog-button-data' : 'student-dialog-button-no-data'}
                     variant='contained'
-                    onClick={handleSubmitGroupStudents}
-                    color='primary'
-                    disabled={setDisabledMoveToGroupBtn()}
-                    title={i18n.t('move_to_group_title')}
-                >
-                    {i18n.t('common:move_to_group_title')}
-                </Button>
-                :null}
-                <Button
-                    className={students.length !== 0?'student-dialog-button-data':'student-dialog-button-no-data'}
-                    variant="contained"
                     onClick={() => onClose('')}
-                    color="primary"
+                    color='primary'
                     title={i18n.t('close_title')}
                 >
                     {i18n.t('common:close_title')}
@@ -265,7 +266,7 @@ ShowStudentsDialog.propTypes = {
     open: PropTypes.bool.isRequired
 };
 const mapStateToProps = state => ({
-   //currentGroup:state.groups.currentGroup
+    //currentGroup:state.groups.currentGroup
 });
 
 export default connect(mapStateToProps, {})(ShowStudentsDialog);
