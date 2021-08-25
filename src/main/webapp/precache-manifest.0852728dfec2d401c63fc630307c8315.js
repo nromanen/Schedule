e226d5a4fb28eacf0ca452efe424fa8c05000561
nromanen <nromanen.softserveinc@gmail.com>
2021-08-25 14:33:40 +0300

    before offering

---
+++ src/main/webapp/precache-manifest.0852728dfec2d401c63fc630307c8315.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66879e0ec5aac26f86753effe84d1adf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8638c486461640b9bb74",
+    "url": "/static/css/main.5a617d12.chunk.css"
+  },
+  {
+    "revision": "ebb63f7abdf5dc34a386",
+    "url": "/static/js/2.da436eb3.chunk.js"
+  },
+  {
+    "revision": "fde8a65b8d307872c8c44f6de8ee07de",
+    "url": "/static/js/2.da436eb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8638c486461640b9bb74",
+    "url": "/static/js/main.b424eb07.chunk.js"
+  },
+  {
+    "revision": "d6634c61822753d83409",
+    "url": "/static/js/runtime-main.c3b6c623.js"
+  }
+]);
