45b1ade099fcf1b1e77118c32eb8321dab97f9f9
Nastia <gerasymchuk88888888@gmail.com>
2021-08-26 10:55:29 +0300

    code refactoring

--- frontend/src/components/SemesterForm/SemesterForm.js
+++ frontend/src/components/SemesterForm/SemesterForm.js
@@ -56,8 +56,8 @@ let AddSemesterForm = props => {
     } = props;
     const [openGroupDialog, setOpenGroupDialog] = useState(false);
     useEffect(() => {
-        const {semester_groups}=semester;
-        if (semester_groups !== undefined&&semester_groups!==null) {
+        const { semester_groups } = semester;
+        if (semester_groups !== undefined && semester_groups !== null) {
             setSelectedGroups(getGroupOptions(semester_groups));
         }
     }, [semester.id]);
