49534db452a5aa32d4bfd99859a04e6ffff2f260
Nastia <gerasymchuk88888888@gmail.com>
2021-08-28 22:31:38 +0300

    change teacher output

--- frontend/src/helper/renderTeacher.js
+++ frontend/src/helper/renderTeacher.js
@@ -1,26 +1,40 @@
-export const getFirstLetter=(word)=>{
-    return word!==null?`${word.charAt(0)}.`:"";
-}
-export const getTeacherName=( teacher )=>{
-    const {name,surname,patronymic}=teacher;
+const parseShortPosition = (position) => {
+    const teacherPosition = new Map();
+    teacherPosition.set('доцент', 'доц.');
+    teacherPosition.set('асистент', 'ac.');
+    teacherPosition.set('професор', 'проф.');
+    return teacherPosition.get(position);
+
+};
+export const getFirstLetter = (word) => {
+    return word !== null ? `${word.charAt(0)}.` : '';
+};
+export const getTeacherName = (teacher) => {
+    const { name, surname, patronymic } = teacher;
     return `${surname} ${getFirstLetter(name)} ${getFirstLetter(patronymic)}`;
 
-}
-export const getTeacherFullName=( teacher )=>{
-    const {name,surname,patronymic}=teacher;
+};
+export const getTeacherFullName = (teacher) => {
+    const { name, surname, patronymic } = teacher;
     return `${surname} ${name} ${patronymic}`;
 
-}
-export const getTeacherForSite=(teacher)=>{
-    const {name,surname,patronymic,position}=teacher;
+};
+export const getTeacherForSite = (teacher) => {
+    const { name, surname, patronymic, position } = teacher;
     return `${position} ${surname} ${getFirstLetter(name)} ${getFirstLetter(patronymic)}\n`;
 
-}
+};
 export const handleTeacherInfo = (teacher) => {
-    const {name,surname,patronymic,position}=teacher;
+    const { name, surname, patronymic, position } = teacher;
     return `${surname} ${name} ${patronymic} (${position})`;
 };
 export const getTeacherWithPosition = (teacher) => {
-    const {name,surname,patronymic,position}=teacher;
+    const { name, surname, patronymic, position } = teacher;
     return `${position} ${surname} ${name} ${patronymic}`;
 };
+export const getTeacherShortPosition = (teacher) => {
+    const { name, surname, patronymic, position } = teacher;
+    return `${parseShortPosition(position)} ${surname} ${getFirstLetter(name)} ${getFirstLetter(patronymic)}\n`;
+
+};
+
