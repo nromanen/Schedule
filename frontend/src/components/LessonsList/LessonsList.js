c68301f4bb4c5036b23386e63c3565c0df34865b
Oleksandr Yashchemskyi <yashchemskyio@gmail.com>
2021-10-19 15:39:51 +0300

    fix: divided LessonsList and corrected semester reducer

--- frontend/src/components/LessonsList/LessonsList.js
+++ frontend/src/components/LessonsList/LessonsList.js
@@ -1,74 +1,16 @@
 import React from 'react';
-import { Trans, useTranslation } from 'react-i18next';
-import { FaEdit, FaUserPlus } from 'react-icons/fa';
-import { MdContentCopy } from 'react-icons/all';
-import { MdDelete } from 'react-icons/md';
-
-import Card from '../../share/Card/Card';
-import { getTeacherName } from '../../helper/renderTeacher';
-import { getShortTitle } from '../../helper/shortTitle';
-
-import { firstStringLetterCapital } from '../../helper/strings';
-import { FORM_GROUPED_LABEL } from '../../constants/translationLabels/formElements';
-import { COPY_LESSON, DELETE_LESSON, EDIT_LESSON } from '../../constants/translationLabels/common';
 import './LessonsList.scss';
+import LessonsCard from './LessonsCard';
 
 const LessonsList = (props) => {
-    const { lessons, onCopyLesson, onSelectLesson, onClickOpen } = props;
-    const { t } = useTranslation(['common', 'formElements']);
+    const { lessons, ...rest } = props;
 
-    const MAX_LENGTH = 50;
-    const getTitle = (lesson) => {
-        return `${firstStringLetterCapital(lesson.subjectForSite)} ${t(
-            `lesson_type_${lesson.lessonType.toLowerCase()}_label`,
-            { ns: 'formElements' },
-        )}`;
-    };
     return (
-        <div>
-            <section className="container-flex-wrap lesson-container">
-                {lessons.map((lesson) => (
-                    <Card additionClassName="done-card" key={lesson.id}>
-                        <div className="cards-btns">
-                            {lesson.grouped && (
-                                <FaUserPlus
-                                    title={t(FORM_GROUPED_LABEL, { ns: 'formElements' })}
-                                    className="svg-btn copy-btn align-left info-btn"
-                                />
-                            )}
-                            <MdContentCopy
-                                title={t(COPY_LESSON)}
-                                className="svg-btn copy-btn"
-                                onClick={() => onCopyLesson(lesson)}
-                            />
-                            <FaEdit
-                                title={t(EDIT_LESSON)}
-                                className="svg-btn edit-btn"
-                                onClick={() => onSelectLesson(lesson.id)}
-                            />
-                            <MdDelete
-                                title={t(DELETE_LESSON)}
-                                className="svg-btn delete-btn"
-                                onClick={() => onClickOpen(lesson.id)}
-                            />
-                        </div>
-                        <p className="title">{getShortTitle(getTitle(lesson), MAX_LENGTH)}</p>
-                        <p>{getTeacherName(lesson.teacher)}</p>
-                        <p>
-                            <Trans
-                                i18nKey="hour"
-                                count={lesson.hours}
-                                ns="common"
-                                components={[<strong key="strong" />]}
-                            >
-                                {{ count: lesson.hours }}
-                            </Trans>
-                        </p>
-                        <input value={lesson.linkToMeeting} disabled />
-                    </Card>
-                ))}
-            </section>
-        </div>
+        <section className="container-flex-wrap lesson-container">
+            {lessons.map((lesson) => (
+                <LessonsCard lesson={lesson} key={lesson.id} {...rest} />
+            ))}
+        </section>
     );
 };
 
