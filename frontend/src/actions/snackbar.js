eab07ebbaf899673feb3be1f8118d1e13306971f
Oleksandr Yashchemskyi <yashchemskyio@gmail.com>
2021-10-28 01:22:26 +0300

    created actions success/error Snackbar

--- frontend/src/actions/snackbar.js
+++ frontend/src/actions/snackbar.js
@@ -1,8 +1,31 @@
 import * as actionTypes from './actionsType';
+import { snackbarTypes } from '../constants/snackbarTypes';
 
 export const setOpenSnackbar = (res) => {
     return {
         type: actionTypes.SET_OPEN_SNACKBAR,
         result: res,
+    };
+};
+
+export const setOpenSuccessSnackbar = (message) => {
+    return {
+        type: actionTypes.SET_OPEN_SNACKBAR,
+        result: {
+            isOpen: true,
+            type: snackbarTypes.SUCCESS,
+            message,
+        },
+    };
+};
+
+export const setOpenErrorSnackbar = (message) => {
+    return {
+        type: actionTypes.SET_OPEN_SNACKBAR,
+        result: {
+            isOpen: true,
+            type: snackbarTypes.ERROR,
+            message,
+        },
     };
 };
