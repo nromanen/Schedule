62768a5620989b12fe00e1e5658c74e51768f1d1
Viktoriia <viktoriia.kosovan@gmail.com>
2021-11-01 18:34:33 +0200

    refactor: SemesterCopy component and divide Semester Select to separate file

--- frontend/src/components/Semester/SemesterCopyForm/SemesterCopyForm.js
+++ frontend/src/components/Semester/SemesterCopyForm/SemesterCopyForm.js
@@ -1,52 +1,18 @@
 import React from 'react';
-import { Field } from 'redux-form';
 import { useTranslation } from 'react-i18next';
 import Button from '@material-ui/core/Button';
 import { MdPlayArrow } from 'react-icons/md';
-import SelectField from '../../../share/renderedFields/select';
 import './SemesterCopyForm.scss';
-import { required } from '../../../validation/validateFields';
 import {
-    FORM_SEMESTER_LABEL,
     FORM_SEMESTER_COPY_RED_HINT,
     FORM_SEMESTER_COPY_HINT,
 } from '../../../constants/translationLabels/formElements';
 import { TEACHER_SCHEDULE_LABEL } from '../../../constants/translationLabels/common';
+import SemesterSelect from './SemesterSelect';
 
 const SemesterCopyForm = (props) => {
     const { t } = useTranslation('common');
     const { semesterId, semesters, handleSubmit, pristine, submitting, submitButtonLabel } = props;
-
-    const renderSemesterList = () => {
-        const availableSemestersForCopy = semesters.filter(
-            (semester) => semester.id !== semesterId,
-        );
-        if (availableSemestersForCopy && availableSemestersForCopy.length > 1) {
-            return (
-                <>
-                    <Field
-                        id="toSemesterId"
-                        name="toSemesterId"
-                        component={SelectField}
-                        label={t(FORM_SEMESTER_LABEL)}
-                        type="text"
-                        validate={[required]}
-                    >
-                        <option />
-                        {availableSemestersForCopy.map((semester) => (
-                            <option key={semester.id} value={semester.id}>
-                                {semester.description}
-                            </option>
-                        ))}
-                    </Field>
-                </>
-            );
-        }
-        if (availableSemestersForCopy && availableSemestersForCopy.length === 1) {
-            return <p>{availableSemestersForCopy[0].description}</p>;
-        }
-        return null;
-    };
 
     return (
         <section className="copy-form">
@@ -54,7 +20,7 @@ const SemesterCopyForm = (props) => {
             <p className="red-color">{t(FORM_SEMESTER_COPY_RED_HINT)}</p>
             <form onSubmit={handleSubmit}>
                 <div className="form-buttons-container">
-                    {renderSemesterList()}
+                    <SemesterSelect semesterId={semesterId} semesters={semesters} />
                     <Button
                         className="semester-copy-btn"
                         variant="contained"
