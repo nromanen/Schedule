6a5176e271fe244882022ea57a06db734dd3f920
HiroFive <vanohahj5555@gmail.com>
2021-11-24 17:20:50 +0200

    remove useless ReduxFormSelect

--- frontend/src/helper/ReduxFormSelect.js
+++
@@ -1,7 +0,0 @@
-import Select from 'react-select';
-import React from 'react';
-
-export const ReduxFormSelect = (props) => {
-    const { options } = props;
-    return <Select options={options} />;
-};
