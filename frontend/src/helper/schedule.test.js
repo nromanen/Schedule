5cfd8c4823b1820c37fbde6fb11dbd3afae4a10c
Viktoriia <viktoriia.kosovan@gmail.com>
2021-11-25 18:33:05 +0200

    feat: create tests for divideLessonsByOneHourLesson function

--- frontend/src/helper/schedule.test.js
+++ frontend/src/helper/schedule.test.js
@@ -1,21 +1,83 @@
-import { getColorByFullness } from './schedule';
+import { getColorByFullness, divideLessonsByOneHourLesson } from './schedule';
 
-const array = [
+const teacherAndSubjectAreTheSame = [
     {
-        groups: [
-            {
-                group_id: 29,
-                group_name: '12 (101-Б)',
-            },
-        ],
         subject_for_site: 'Web-дизайн',
-        lesson_type: 'LECTURE',
+        teacher_for_site: "Куб'як",
+    },
+    {
+        subject_for_site: 'Web-дизайн',
         teacher_for_site: "Куб'як",
     },
 ];
 
+const teacherTheSame = [
+    {
+        subject_for_site: 'Системи штучного інтелекту',
+        teacher_for_site: 'Мельник',
+    },
+    {
+        lesson_type: 'LECTURE',
+        teacher_for_site: 'Мельник',
+    },
+];
+
+const teacherIsNotTheSame = [
+    {
+        subject_for_site: 'Системи штучного інтелекту',
+        teacher_for_site: 'Мельник',
+    },
+    {
+        subject_for_site: 'Інтелектуальні інформаційні системи',
+        teacher_for_site: "Куб'як",
+    },
+];
+const lessons = [
+    {
+        id: 767,
+        hours: 2,
+    },
+    {
+        id: 784,
+        hours: 3,
+    },
+];
+
+const items = [
+    {
+        lesson: {
+            id: 767,
+            hours: 2,
+        },
+    },
+    {
+        lesson: {
+            id: 772,
+            hours: 2,
+        },
+    },
+];
+
+describe('behavior of divideLessonsByOneHourLesson function', () => {
+    test('it should return lessons with one-hour duration each which length equal amount of all hours of lessons', () => {
+        expect(divideLessonsByOneHourLesson([], lessons).length).toBe(5);
+    });
+    test('it should return lessons with one-hour duration each which length equal amount of all hours of lessons minus amount of items which exist in lessons', () => {
+        expect(divideLessonsByOneHourLesson(items, lessons).length).toBe(4);
+    });
+});
+
 describe('behavior of getColorByFullness function', () => {
-    test('it add all items if search term length === 0', () => {
+    test('it adds  css class "available" if  array of groups is empty', () => {
         expect(getColorByFullness([])).toBe('available');
+    });
+    test('it adds  css class "possible" if  lesson is not the same and teacher is the same', () => {
+        expect(getColorByFullness(teacherTheSame)).toBe('possible');
+    });
+    test('it adds css class "not-allow" if teacher is not the same', () => {
+        expect(getColorByFullness(teacherIsNotTheSame)).toBe('not-allow');
+    });
+    test('it adds css class "allow" if lesson and teacher are the same', () => {
+        expect(getColorByFullness(teacherAndSubjectAreTheSame)).toBe('allow');
     });
 });
