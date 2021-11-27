be65e99fb174836edc4719351059c528fa9364ab
Niknikson <niknikson34@gmail.com>
2021-11-27 16:50:22 +0300

    feat:  add  Autocomplete to addGroupForm

--- frontend/src/components/AddGroupForm/AddGroupForm.js
+++ frontend/src/components/AddGroupForm/AddGroupForm.js
@@ -1,11 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Field } from 'redux-form';
 import Button from '@material-ui/core/Button';
 import { useTranslation } from 'react-i18next';
 
 import './AddGroupForms.scss';
-import { Autocomplete } from '@material-ui/lab';
-import TextField from '@material-ui/core/TextField';
+
 import renderTextField from '../../share/renderedFields/input';
 import { required, uniqueGroup, minLengthValue } from '../../validation/validateFields';
 import { getClearOrCancelTitle, setDisableButton } from '../../helper/disableComponent';
@@ -17,8 +16,9 @@ import {
     SAVE_BUTTON_LABEL,
     FORM_GROUP_LABEL_AFTER,
 } from '../../constants/translationLabels/formElements';
+import { renderAutocompleteField } from '../../helper/renderAutocompleteField';
 
-export const AddGroup = (props) => {
+export const AddGroup = props => {
     const {
         clearGroupStart,
         submitGroupStart,
@@ -32,37 +32,36 @@ export const AddGroup = (props) => {
         group,
     } = props;
     const { t } = useTranslation('formElements');
-    const [afterIdGroup, setAfterIdGroup] = useState(null);
 
-    const getAfterIdGroup = () => {
-        const groupIndex = groups.findIndex(({ id }) => id === group.id);
-        const afterId = groups.find((item, index) => index === groupIndex - 1);
-        setAfterIdGroup(afterId);
-    };
+    const removeCurrentGroup = () => groups.filter(el => el.id !== group.id);
+
+    const groupsForAfterId = group.id ? removeCurrentGroup() : groups;
 
     useEffect(() => {
+        const groupIndex = groups.findIndex(({ id }) => id === group.id);
+        const afterId = groups.find((item, index) => index === groupIndex - 1);
         if (group.id) {
             initialize({
                 id: group.id,
                 title: group.title,
+                afterId,
             });
-            getAfterIdGroup();
         } else {
             initialize();
         }
     }, [group.id]);
 
-    const submitGroup = (data) => {
-        const afterId = afterIdGroup ? afterIdGroup.id : null;
+    const submitGroup = data => {
+        const afterId = data.afterId ? data.afterId.id : null;
         submitGroupStart({ ...data, disable: false, afterId });
         setGroup({});
-        setAfterIdGroup(null);
+        // console.log(data);
+        // console.log({ ...data, disable: false, afterId });
     };
 
     const onReset = () => {
         setGroup({});
         clearGroupStart();
-        setAfterIdGroup(null);
     };
 
     return (
@@ -71,36 +70,23 @@ export const AddGroup = (props) => {
                 {group.id ? t(EDIT_TITLE) : t(CREATE_TITLE)}
                 {t(GROUP_Y_LABEL)}
             </h3>
-            <form onSubmit={handleSubmit((data) => submitGroup(data))}>
-                <Autocomplete
-                    id="group"
-                    value={afterIdGroup}
-                    options={groups}
-                    className="group-lesson"
-                    clearOnEscape
-                    openOnFocus
-                    getOptionLabel={(option) => option.title}
-                    onChange={(_, newValue) => {
-                        setAfterIdGroup(newValue);
-                    }}
-                    renderInput={(params) => (
-                        <TextField
-                            className="textField"
-                            {...params}
-                            label={t(FORM_GROUP_LABEL_AFTER)}
-                            margin="normal"
-                        />
-                    )}
-                />
-
+            <form onSubmit={handleSubmit(data => submitGroup(data))}>
                 <Field
                     className="form-field"
                     name="title"
                     id="title"
                     label={`${t(GROUP_LABEL)}:`}
                     component={renderTextField}
-                    validate={[required, minLengthValue, uniqueGroup]}
+                    validate={[required, minLengthValue]}
                 />
+                <Field
+                    name="afterId"
+                    component={renderAutocompleteField}
+                    label={t(FORM_GROUP_LABEL_AFTER)}
+                    type="text"
+                    values={groupsForAfterId}
+                    getOptionLabel={item => (item ? item.title : '')}
+                ></Field>
                 <div className="form-buttons-container">
                     <Button
                         size="small"
