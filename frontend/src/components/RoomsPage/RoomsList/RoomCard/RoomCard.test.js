ff96caae2a2b98a1ea059f9120d546500414c743
Viktoriia <viktoriia.kosovan@gmail.com>
2021-11-30 17:37:46 +0200

    fix: test for RoomCard

--- frontend/src/components/RoomsPage/RoomsList/RoomCard/RoomCard.test.js
+++ frontend/src/components/RoomsPage/RoomsList/RoomCard/RoomCard.test.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import { GiSightDisabled, IoMdEye } from 'react-icons/all';
-import { MdDelete } from 'react-icons/md';
-import { FaEdit } from 'react-icons/fa';
+import { mount } from 'enzyme';
 import RoomCard from './RoomCard';
 
 const showConfirmDialog = jest.fn();
@@ -23,39 +20,47 @@ const props = {
 
 describe('behavior of RoomCard Component when room is disabled', () => {
     let wrapper;
+
     beforeEach(() => {
-        wrapper = shallow(<RoomCard {...props} />);
+        wrapper = mount(<RoomCard {...props} />);
     });
+
+    afterEach(() => wrapper.unmount());
+
     it('should render GiSightDisabled and MdDelete icons if disabled is true', () => {
-        expect(wrapper.find(GiSightDisabled)).toHaveLength(1);
-        expect(wrapper.find(MdDelete)).toHaveLength(1);
+        expect(wrapper.find('GiSightDisabled')).toHaveLength(1);
+        expect(wrapper.find('MdDelete')).toHaveLength(1);
     });
     it('should call showConfirmDialog when click to GiSightDisabled icon', () => {
-        wrapper.find(GiSightDisabled).simulate('click');
-        expect(props.showConfirmDialog).toHaveBeenCalled();
+        wrapper.find('GiSightDisabled').simulate('click');
+        expect(props.showConfirmDialog.mock.calls.length).toEqual(1);
     });
     it('should call showConfirmDialog when click to MdDelete icon', () => {
-        wrapper.find(MdDelete).simulate('click');
-        expect(props.showConfirmDialog).toHaveBeenCalled();
+        wrapper.find('MdDelete').simulate('click');
+        expect(props.showConfirmDialog.mock.calls.length).toEqual(1);
     });
 });
 
 describe('behavior of RoomCard Component when room is not disabled', () => {
     let wrapper;
+
     beforeEach(() => {
-        wrapper = shallow(<RoomCard {...props} isDisabled={false} />);
+        wrapper = mount(<RoomCard {...props} isDisabled={false} />);
     });
+
+    afterEach(() => wrapper.unmount());
+
     it('should render IoMdEye, FaEdit and MdDelete icons if disabled is false', () => {
-        expect(wrapper.find(IoMdEye)).toHaveLength(1);
-        expect(wrapper.find(FaEdit)).toHaveLength(1);
-        expect(wrapper.find(MdDelete)).toHaveLength(1);
+        expect(wrapper.find('IoMdEye')).toHaveLength(1);
+        expect(wrapper.find('FaEdit')).toHaveLength(1);
+        expect(wrapper.find('MdDelete')).toHaveLength(1);
     });
     it('should call showConfirmDialog when click to IoMdEye icon', () => {
-        wrapper.find(IoMdEye).simulate('click');
-        expect(props.showConfirmDialog).toHaveBeenCalled();
+        wrapper.find('IoMdEye').simulate('click');
+        expect(props.showConfirmDialog.mock.calls.length).toEqual(1);
     });
     it('should call setSelectRoom when click to FaEdit icon', () => {
-        wrapper.find(FaEdit).simulate('click');
-        expect(props.setSelectRoom).toHaveBeenCalled();
+        wrapper.find('FaEdit').simulate('click');
+        expect(props.setSelectRoom.mock.calls.length).toEqual(1);
     });
 });
