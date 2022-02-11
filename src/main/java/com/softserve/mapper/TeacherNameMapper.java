619cb70d6fcda1cf709ec84390ceefb7c4e4c0c4
Oleksandr <sane4q@gamil.com>
2022-02-11 15:30:44 +0200

    Change TeacherNameMapper and add it to LessonBaseDTO

--- src/main/java/com/softserve/mapper/TeacherNameMapper.java
+++ src/main/java/com/softserve/mapper/TeacherNameMapper.java
@@ -1,11 +1,16 @@
 package com.softserve.mapper;
 
-import com.softserve.dto.TeacherDTO;
+import com.softserve.dto.TeacherNameDTO;
 import com.softserve.entity.Teacher;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface TeacherNameMapper {
-    TeacherDTO teacherDTOToTeacher(Teacher teacher);
-    Teacher teacherToTeacherDTO(TeacherDTO teacherNameDTO);
+    TeacherNameDTO teacherDTOToTeacher(Teacher teacher);
+    @Mapping(target = "position", ignore = true)
+    @Mapping(target = "userId", ignore = true)
+    @Mapping(target = "department", ignore = true)
+    @Mapping(target = "disable", ignore = true)
+    Teacher teacherToTeacherDTO(TeacherNameDTO teacherNameDTO);
 }
