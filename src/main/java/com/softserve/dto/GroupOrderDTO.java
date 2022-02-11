0fee6e0fff55d70b3422bc7a2383f87b0dd91cbb
Oleksandr <sane4q@gamil.com>
2022-02-11 15:30:06 +0200

    Add callSuper=false

--- src/main/java/com/softserve/dto/GroupOrderDTO.java
+++ src/main/java/com/softserve/dto/GroupOrderDTO.java
@@ -5,10 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.EqualsAndHashCode;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
 public class GroupOrderDTO extends GroupDTO{
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
