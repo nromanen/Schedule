4b8881639440eb1ecfec246175c112e18bd44517
Voland <vchornyy@ukr.net>
2022-02-15 10:57:24 +0200

    Fixed incorrect data types for Repository classes

--- src/main/java/com/softserve/repository/impl/LessonRepositoryImpl.java
+++ src/main/java/com/softserve/repository/impl/LessonRepositoryImpl.java
@@ -273,7 +273,7 @@ public class LessonRepositoryImpl extends BasicRepositoryImpl<Lesson, Long> impl
     @Override
     protected boolean checkReference(Lesson lesson) {
         log.info("In checkReference(lesson = [{}])", lesson);
-        long count = sessionFactory.getCurrentSession().createQuery
+        Long count = sessionFactory.getCurrentSession().createQuery
                 (COUNT_QUERY, Long.class)
                 .setParameter("lessonId", lesson.getId())
                 .getSingleResult();
