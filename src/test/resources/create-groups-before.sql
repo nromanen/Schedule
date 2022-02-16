ced5258dcce2adcab1ceb906278061094c08db28
Voland <vchornyy@ukr.net>
2022-02-16 17:10:23 +0200

    Fixed tests

--- src/test/resources/create-groups-before.sql
+++ src/test/resources/create-groups-before.sql
@@ -10,5 +10,8 @@ insert into "public".groups(id, title, disable, sorting_order) values
 (5,'555', true, null),
 (6, '666', false, 1);
 
-insert into "public".students(id, email, name, patronymic, surname, group_id) values
-(4, 'zzz1@gmail.com', 'Name One', 'Patron One', 'Surname One', 4);
+insert into "public" .users(id, email, password, role) values
+(1, 'zzz1@gmail.com', 'Zzzzz123!', 'ROLE_USER');
+
+insert into "public".students(id, name, patronymic, surname, group_id, user_id) values
+(4, 'Name One', 'Patron One', 'Surname One', 4, 1);
