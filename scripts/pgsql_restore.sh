698bc609b389d8b669622ff6a90338e0b0b2907a
gluhanuk98 <gluhanuk98@gmail.com>
2022-06-24 15:10:50 +0300

    Change properties for work

--- scripts/pgsql_restore.sh
+++ scripts/pgsql_restore.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #change on your path to PostgreSQL
-pathA=/c/Program\ Files/PostgreSQL/13/bin
+pathA=/E/DB/bin
 export PATH=$PATH:$pathA
 
 #write your password
-PGPASSWORD=
+PGPASSWORD=root
 export PGPASSWORD
 
 #change the path to the file from which will be made restore
@@ -13,13 +13,13 @@ pathB=../backup/
 #write this on the command line as the first parameter
 filename=$1
 #write your user
-dbUser=
+dbUser=postgres
 #write your database
-database=
+database=schedule
 #write your host
-host=
+host=localhost
 #write your port
-port=
+port=5432
 
 psql -U $dbUser -h $host -p $port -d $database -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
 psql --set ON_ERROR_STOP=off -U $dbUser -h $host -p $port -d $database -1 -f $pathB$filename
